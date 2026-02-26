28128fc625af5ea81d5a25796ef94f3fc9dc0f57
Mokhmad Noutsoulkhanov <mokhmad.noutsoulkhanov@oclock.school>
2026-02-26 11:48:58 +0100

    migration fixed

--- migrations/config.cjs
+++ migrations/config.cjs
@@ -2,12 +2,12 @@ require('dotenv/config');
 
 module.exports = {
   development: {
-    url: process.env.PG_URL,
+    url: process.env.PG_URL || process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
   },
   production: {
-    url: process.env.PG_URL,
+    url: process.env.PG_URL || process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
